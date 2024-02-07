9d48a4dffcabcb552bfc694b3d0bc9004d68f7b7
Rastsislau Rudziankou <rudenkovrostislav1@gmail.com>
2024-02-07 18:59:36 +0100

    Prioritizing & Disabling Tests

--- src/PriorityExample.java
+++ src/PriorityExample.java
@@ -2,25 +2,25 @@ import org.testng.annotations.Test;
 
 public class PriorityExample {
 
-    @Test(priority = 0)
+    @Test(priority = 1)
     void testOne()
     {
         System.out.println("This is Test 1");
     }
 
-    @Test(priority = 1)
+    @Test(priority = 2)
     void testTwo()
     {
         System.out.println("This is Test 2");
     }
 
-    @Test(priority = 2)
+    @Test(priority = 3)
     void testThree()
     {
         System.out.println("This is Test 3");
     }
 
-    @Test(priority = 3, enabled = false)
+    @Test(priority = 4, enabled = false)
     void testFour()
     {
         System.out.println("This is Test 4");
