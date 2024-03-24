7bd88c15188a3732975a13fe5cf7350ddd004e89
Rastsislau Rudziankou <rudenkovrostislav1@gmail.com>
2024-03-24 11:14:31 +0100

    Interface Concept

---
+++ src/oop/day16/Interfacedemo.java
@@ -0,0 +1,50 @@
+package oop.day16;
+
+interface Shape
+{
+    int length = 10; // final & static
+    int width = 20; // final & static
+
+    void circle(); // abstract method
+
+    static void rectangle()
+    {
+        System.out.println("this is rectangle - static method ");
+    }
+
+    default void square()
+    {
+        System.out.println("this is square - default method ");
+    }
+}
+
+
+public class Interfacedemo implements Shape
+{
+    public void circle() {
+        System.out.println("this is circle - abstract method");
+    }
+
+    void triangle()
+    {
+        System.out.println("this is triangle...");
+    }
+
+    public static void main(String[] args) {
+
+        // Scenario 1
+        Interfacedemo idobj = new Interfacedemo();
+
+        idobj.circle();    // abstract method
+        idobj.square();    // default method
+        Shape.rectangle(); // static method can directly access from interface
+        idobj.triangle();  // this method belongs to Interfacedemo class only
+
+        // Scenario 2
+        Shape sh = new Interfacedemo();
+        sh.circle(); // abstract method
+        sh.square(); // default method
+        Shape.rectangle(); // static method can directly access from interface
+
+    }
+}
