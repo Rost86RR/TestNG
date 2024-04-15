38a31a655bd12f18ba046b6851e21e9d67adcfb1
Rastsislau Rudziankou <rudenkovrostislav1@gmail.com>
2024-04-15 20:29:14 +0200

    Wrapper Classes 1.1

--- src/javva/day17/DataConversionMethods.java
+++ src/javva/day17/DataConversionMethods.java
@@ -11,5 +11,39 @@ public class DataConversionMethods {
         String s1 = "10";
         String s2 = "20";
         System.out.println(Integer.parseInt(s1)+Integer.parseInt(s2));
+
+        // String --> double ------------------------------------------------------------------
+
+        String s3 = "10.5";
+        String s4 = "20.0";
+        System.out.println(Double.parseDouble(s3) + Double.parseDouble(s4));
+
+        // String --> Boolean -----------------------------------------------------------------
+
+        String s5 = "true";
+        String s6 = "false";
+        String s7 = "anything";
+        System.out.println(Boolean.parseBoolean(s5));
+        System.out.println(Boolean.parseBoolean(s6));
+        System.out.println(Boolean.parseBoolean(s7));
+
+        // int, bool, double, char ---> String ------------------------------------------------
+
+        int a = 10;
+        boolean b = true;
+        char c = 'A';
+        double d = 10.5;
+
+        String s8 = String.valueOf(a);
+        System.out.println(s8);
+
+        String s9 = String.valueOf(b);
+        System.out.println(s9);
+
+        String s10 = String.valueOf(c);
+        System.out.println(s10);
+
+        String s11 = String.valueOf(d);
+        System.out.println(s11);
     }
 }
