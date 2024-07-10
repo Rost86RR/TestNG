2bd7bfe3b4d42b3223549b3f8a99ee471103aa3b
Rastsislau Rudziankou <rudenkovrostislav1@gmail.com>
2024-07-10 20:09:08 +0200

    TypeCastingConcept1

---
+++ src/javva/day19/TypeCastingConcept.java
@@ -0,0 +1,23 @@
+package javva.day19;
+
+//Upcasting - converting value from smaller --> larger
+// int -----> long
+// float ---> double
+
+
+//Downcasting - converting value from larger --> smaller
+// long -----> int
+// double ---> float
+
+public class TypeCastingConcept {
+
+    public static void main(String[] args) {
+
+        //upcasting -automatic ------ smaller to larger
+        int intvalue = 100;
+        long longvalue = intvalue;
+        System.out.println(longvalue);
+
+
+    }
+}
